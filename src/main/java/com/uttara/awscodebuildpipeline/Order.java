c937035383724b3f14b85a1073e32a4da6764d65
umesha-hb <umesha.kit@gmail.com>
2023-05-18 21:45:24 +0530

    added buildspec.yml

---
+++ src/main/java/com/uttara/awscodebuildpipeline/Order.java
@@ -0,0 +1,21 @@
+package com.uttara.awscodebuildpipeline;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Order {
+    private int id;
+    private String name;
+    private int quantity;
+
+    private long price;
+
+
+}
---
+++ src/test/java/com/uttara/awscodebuildpipeline/AwsCodeBuildPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttara.awscodebuildpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodeBuildPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
