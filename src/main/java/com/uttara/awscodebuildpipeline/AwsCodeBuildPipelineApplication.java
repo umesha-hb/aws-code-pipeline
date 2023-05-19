f912461dbec5ec372405e5728516e1971da0f74b
umesha-hb <umesha.kit@gmail.com>
2023-05-19 16:58:45 +0530

    removed sorting

--- src/main/java/com/uttara/awscodebuildpipeline/AwsCodeBuildPipelineApplication.java
+++ src/main/java/com/uttara/awscodebuildpipeline/AwsCodeBuildPipelineApplication.java
@@ -22,7 +22,7 @@ public class AwsCodeBuildPipelineApplication {
     @GetMapping
 
     public List<Order> fetchOrders() {
-        return orderDao.getOrders().stream().sorted(Comparator.comparing(Order::getPrice).reversed()).collect(Collectors.toList());
+        return orderDao.getOrders();
     }
 
     public static void main(String[] args) {
